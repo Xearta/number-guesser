6182216381309cb8522820ea69ca128cbb2e7ff5
Tanner Townsend <rsmcwow@gmail.com>
2022-10-30 13:28:35 -0400

    Add gameplay functionality; game complete

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
 
+const compareGuesses = (humanGuess, computerGuess, target) => {
+  const humanDifference = Math.abs(target - humanGuess);
+  const computerDifference = Math.abs(target - computerGuess);
+  return humanDifference <= computerDifference;
+};
+
+const updateScore = (winner) => {
+  if (winner === "human") {
+    humanScore++;
+  } else if (winner === "computer") {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => currentRoundNumber++;
